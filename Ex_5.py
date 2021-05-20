44401393c6b1263ea97981e10471830cd7818761
shefreyn <42711053+shefreyn@users.noreply.github.com>
2021-05-20 18:36:36 +0530

    Create Ex_5.py

---
+++ Ex_5.py
@@ -0,0 +1,10 @@
+class GetnPrint:
+    def __init__():
+        getStr = ''
+    def getString():
+        getStr = input("Enter a String")
+    def printString():
+        print(GetnPrint.getString())
+
+GetnPrint.getString()
+GetnPrint.printString()
